1e8d0c6a947418af9ee075f4945434288c860899
LeicaSimile <leicasimile@gmail.com>
2018-01-30 14:12:19 -0500

    Add event handler for btnSubway

--- week02/IntentApp/app/src/main/java/com/leicasimile/comp304/intentapp/MainActivity.java
+++ week02/IntentApp/app/src/main/java/com/leicasimile/comp304/intentapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.leicasimile.comp304.intentapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -14,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextView txtMessage = (TextView) findViewById(R.id.txtMessage);
         txtMessage.setText(R.string.message);
+    }
+
+    private void Send_Message(View v) {
+        if (v.getId() == R.id.btnSubway) {
+            Intent intent = new Intent(MainActivity.this, Subway.class);
+            startActivity(intent);
+        }
     }
 
 }
