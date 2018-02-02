b3c0b385839a3844846c59f5cdc874749a2e1728
acatala2 <tumthe3@gmail.com>
2018-02-02 13:40:32 -0500

    Add handler for selecting fragment

--- week02/FragmentApp/app/src/main/java/com/leicasimile/comp304/fragmentapp/FragmentActivity.java
+++ week02/FragmentApp/app/src/main/java/com/leicasimile/comp304/fragmentapp/FragmentActivity.java
@@ -1,7 +1,11 @@
 package com.leicasimile.comp304.fragmentapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FragmentActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class FragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+    }
+
+    public void Fragment_Selection(View v) {
+        Fragment fragment = new One();
+
+        if (v.getId() == R.id.btn_one)
+            fragment = new One();
+        else if (v.getId() == R.id.btn_two)
+            fragment = new Two();
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment, fragment);
+        transaction.commit();
     }
 }
