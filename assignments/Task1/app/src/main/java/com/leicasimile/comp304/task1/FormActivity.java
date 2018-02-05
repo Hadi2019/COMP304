78ec01a2056528613baef149d91330ad5cabdc3e
LeicaSimile <leicasimile@gmail.com>
2018-02-04 21:44:50 -0500

    Add handler for btn_submit, start InfoActivity

--- assignments/Task1/app/src/main/java/com/leicasimile/comp304/task1/FormActivity.java
+++ assignments/Task1/app/src/main/java/com/leicasimile/comp304/task1/FormActivity.java
@@ -1,7 +1,10 @@
 package com.leicasimile.comp304.task1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+    }
+
+    public void btnSubmit_click(View view) {
+        EditText edit_student =  findViewById(R.id.edit_student);
+        EditText edit_program =  findViewById(R.id.edit_program);
+        EditText edit_duration =  findViewById(R.id.edit_duration);
+        String student = edit_student.getText().toString();
+        String program = edit_program.getText().toString();
+        String duration = edit_duration.getText().toString();
+
+        Intent i = new Intent(this, InfoActivity.class);
+        i.putExtra("student", student);
+        i.putExtra("program", program);
+        i.putExtra("duration", duration);
+        startActivity(i);
     }
 }
