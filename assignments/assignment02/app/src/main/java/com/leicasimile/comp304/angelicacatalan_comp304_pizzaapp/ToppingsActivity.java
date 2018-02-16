6d498f4ad9f4d85988d7f5a83adb759d2aec3fe2
LeicaSimile <leicasimile@gmail.com>
2018-02-15 21:34:47 -0500

    Add radio button ids

---
+++ assignments/assignment02/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_pizzaapp/ToppingsActivity.java
@@ -0,0 +1,13 @@
+package com.leicasimile.comp304.angelicacatalan_comp304_pizzaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToppingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toppings);
+    }
+}
