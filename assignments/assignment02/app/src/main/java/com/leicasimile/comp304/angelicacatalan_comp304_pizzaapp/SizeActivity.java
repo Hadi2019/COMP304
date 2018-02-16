24d50e27305856629ba2a1c5860cfae53ac422ab
LeicaSimile <leicasimile@gmail.com>
2018-02-16 10:03:11 -0500

    Add colour for next button

--- assignments/assignment02/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_pizzaapp/SizeActivity.java
+++ assignments/assignment02/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_pizzaapp/SizeActivity.java
@@ -2,6 +2,7 @@ package com.leicasimile.comp304.angelicacatalan_comp304_pizzaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SizeActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SizeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_size);
+
+        Button btnNext = findViewById(R.id.size_btnNext);
+        btnNext.setBackgroundColor(getResources().getColor(R.color.nextButton));
     }
 }
--- assignments/assignment02/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_pizzaapp/ToppingsActivity.java
+++ assignments/assignment02/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_pizzaapp/ToppingsActivity.java
@@ -2,6 +2,7 @@ package com.leicasimile.comp304.angelicacatalan_comp304_pizzaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ToppingsActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ToppingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toppings);
+
+        Button btnNext = findViewById(R.id.toppings_btnNext);
+        btnNext.setBackgroundColor(getResources().getColor(R.color.nextButton));
     }
 }
