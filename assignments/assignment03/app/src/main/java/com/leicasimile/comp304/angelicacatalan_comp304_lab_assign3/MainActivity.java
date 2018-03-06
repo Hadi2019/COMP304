ba0b5f242d55f3c7cc20a576262764f767ecdd4e
LeicaSimile <leicasimile@gmail.com>
2018-03-06 14:24:47 -0500

    Bind tasks to listview

--- assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/MainActivity.java
+++ assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/MainActivity.java
@@ -1,13 +1,26 @@
 package com.leicasimile.comp304.angelicacatalan_comp304_lab_assign3;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+    String[] tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listTasks = getListView();
+        listTasks.setChoiceMode(ListView.CHOICE_MODE_NONE);
+        listTasks.setTextFilterEnabled(true);
+
+        tasks = getResources().getStringArray(R.array.tasks);
+
+        // Bind array to ListView
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tasks));
     }
 }
