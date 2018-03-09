ad827e8fdd2c9ff25b3f2fb7bc52f9088b8103af
LeicaSimile <leicasimile@gmail.com>
2018-03-09 02:05:57 -0500

    Add line thickness values to spinner

--- assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/Task1Activity.java
+++ assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/Task1Activity.java
@@ -2,6 +2,8 @@ package com.leicasimile.comp304.angelicacatalan_comp304_lab_assign3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Task1Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Task1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task1);
+
+        // Set spinner values
+        Spinner spnCardType = findViewById(R.id.task1_spnLineThickness);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.line_thicknesses,
+                android.R.layout.simple_spinner_dropdown_item);
+        spnCardType.setAdapter(adapter);
     }
 }
