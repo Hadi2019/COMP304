5c55cd9be6b765b0ee1cd314c45b549f7f1895c2
LeicaSimile <leicasimile@gmail.com>
2018-03-08 20:39:33 -0500

    Add intents for tasks

--- assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/MainActivity.java
+++ assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/MainActivity.java
@@ -1,8 +1,10 @@
 package com.leicasimile.comp304.angelicacatalan_comp304_lab_assign3;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,5 +24,21 @@ public class MainActivity extends ListActivity {
         // Bind array to ListView
         setListAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, tasks));
+    }
+
+    public void onListItemClick(ListView parent, View v, int position, long id) {
+        Intent i = null;
+        switch (position) {
+            case 0:
+                i = new Intent(this, Task1Activity.class);
+                break;
+            case 1:
+                i = new Intent(this, Task2Activity.class);
+                break;
+            case 2:
+                i = new Intent(this, Task3Activity.class);
+                break;
+        }
+        startActivity(i);
     }
 }
