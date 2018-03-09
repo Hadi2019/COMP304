4ae8dcacd1bf1fd3025584b9aa7d88de671bca25
LeicaSimile <leicasimile@gmail.com>
2018-03-09 03:28:31 -0500

    Add arrow key images

--- assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/Task1Activity.java
+++ assignments/assignment03/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign3/Task1Activity.java
@@ -3,6 +3,8 @@ package com.leicasimile.comp304.angelicacatalan_comp304_lab_assign3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class Task1Activity extends AppCompatActivity {
@@ -17,5 +19,15 @@ public class Task1Activity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.line_thicknesses,
                 android.R.layout.simple_spinner_dropdown_item);
         spnCardType.setAdapter(adapter);
+
+        // Set arrow key images
+        ImageButton imgArrowUp = findViewById(R.id.imgArrowUp);
+        ImageButton imgArrowLeft= findViewById(R.id.imgArrowLeft);
+        ImageButton imgArrowRight = findViewById(R.id.imgArrowRight);
+        ImageButton imgArrowDown = findViewById(R.id.imgArrowDown);
+        imgArrowUp.setImageDrawable(getResources().getDrawable(R.drawable.ic_keyboard_arrow_up_black_24dp));
+        imgArrowLeft.setImageDrawable(getResources().getDrawable(R.drawable.ic_keyboard_arrow_left_black_24dp));
+        imgArrowRight.setImageDrawable(getResources().getDrawable(R.drawable.ic_keyboard_arrow_right_black_24dp));
+        imgArrowDown.setImageDrawable(getResources().getDrawable(R.drawable.ic_keyboard_arrow_down_black_24dp));
     }
 }
