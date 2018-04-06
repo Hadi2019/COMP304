30bea15de3500251babf6fd281c2439c760707fa
LeicaSimile <leicasimile@gmail.com>
2018-04-06 13:31:27 -0400

    Change webview scale

--- week10/WebkitExample/app/src/main/java/com/leicasimile/comp304/webkitexample/MainActivity.java
+++ week10/WebkitExample/app/src/main/java/com/leicasimile/comp304/webkitexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.leicasimile.comp304.webkitexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView wv = findViewById(R.id.main_webview);
+        WebSettings wvSettings = wv.getSettings();
+        wvSettings.setJavaScriptEnabled(true);
         wv.loadUrl("http://www.tomroyal.com/teaandkittens/");
-        wv.setInitialScale(60);
+        wv.setInitialScale(65);
     }
 }
