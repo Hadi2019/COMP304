41d6d42ccd64a90c1924b82ba3b61b4f7938f78a
LeicaSimile <leicasimile@gmail.com>
2018-04-06 13:11:16 -0400

    Load website

--- week10/WebkitExample/app/src/main/java/com/leicasimile/comp304/webkitexample/MainActivity.java
+++ week10/WebkitExample/app/src/main/java/com/leicasimile/comp304/webkitexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.leicasimile.comp304.webkitexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView wv = findViewById(R.id.main_webview);
+        wv.loadUrl("http://www.tomroyal.com/teaandkittens/");
+        wv.setInitialScale(60);
     }
 }
