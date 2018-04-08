89b9dba087a293bb51ed2e6a1f231256dfb26ffb
LeicaSimile <leicasimile@gmail.com>
2018-04-08 19:59:24 -0400

    Add second map

--- assignments/assignment05/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign5/MapActivity.java
+++ assignments/assignment05/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign5/MapActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -19,17 +20,20 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        SupportMapFragment mapNormal = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_normal);
+        mapNormal.getMapAsync(this);
+
+        SupportMapFragment mapSatellite = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_satellite);
+        mapSatellite.getMapAsync(this);
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -38,9 +42,10 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng campus = new LatLng(-34, 151);
+
+
+        mMap.addMarker(new MarkerOptions().position(campus).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(campus));
     }
 }
