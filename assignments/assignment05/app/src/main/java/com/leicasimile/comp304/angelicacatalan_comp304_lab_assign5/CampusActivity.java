b422affb68fdbe5b02ddfacd4f6cd45433ddd475
LeicaSimile <leicasimile@gmail.com>
2018-04-08 17:51:29 -0400

    Add permissions and api key

--- assignments/assignment05/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign5/CampusActivity.java
+++ assignments/assignment05/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign5/CampusActivity.java
@@ -4,10 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CampusActivity extends AppCompatActivity {
+    String[] campuses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
+
+        String college = getIntent().getStringExtra("college");
+        switch (college) {
+            case "Centennial":
+                campuses = getResources().getStringArray(R.array.campuses_centennial);
+                break;
+            case "George Brown":
+                campuses = getResources().getStringArray(R.array.campuses_georgeBrown);
+                break;
+            case "Humber":
+                campuses = getResources().getStringArray(R.array.campuses_humber);
+                break;
+            case "Seneca":
+                campuses = getResources().getStringArray(R.array.campuses_seneca);
+                break;
+            case "Sheridan":
+                campuses = getResources().getStringArray(R.array.campuses_sheridan);
+                break;
+        }
     }
 }
