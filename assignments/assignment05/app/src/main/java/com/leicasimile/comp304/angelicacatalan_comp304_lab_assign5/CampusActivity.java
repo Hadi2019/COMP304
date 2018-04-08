c0103bc33274f8cdb53c055f5b4cd33b848c656f
LeicaSimile <leicasimile@gmail.com>
2018-04-08 19:12:35 -0400

    Add MapActivity

--- assignments/assignment05/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign5/CampusActivity.java
+++ assignments/assignment05/app/src/main/java/com/leicasimile/comp304/angelicacatalan_comp304_lab_assign5/CampusActivity.java
@@ -1,15 +1,32 @@
 package com.leicasimile.comp304.angelicacatalan_comp304_lab_assign5;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class CampusActivity extends AppCompatActivity {
+public class CampusActivity extends ListActivity {
     String[] campuses;
+    ListView lstView;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
+
+        lstView = getListView();
+        TextView textView = new TextView(getApplicationContext());
+        textView.setText(getResources().getString(R.string.campus_txtPrompt));
+
+        lstView.addHeaderView(textView);
+        lstView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        lstView.setTextFilterEnabled(true);
 
         String college = getIntent().getStringExtra("college");
         switch (college) {
@@ -29,5 +46,23 @@ public class CampusActivity extends AppCompatActivity {
                 campuses = getResources().getStringArray(R.array.campuses_sheridan);
                 break;
         }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, campuses);
+        lstView.setAdapter(adapter);
+
+        // Event-handlers
+        lstView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                displayMap(position);
+            }
+        });
+    }
+
+    private void displayMap(int index) {
+        intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
